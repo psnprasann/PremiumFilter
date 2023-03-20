f406611ead49cdc331d4c2db09611ec34c6d3ea4
psnprasann <106792309+psnprasann@users.noreply.github.com>
2023-03-20 08:10:35 +0530

    Create info.py

--- info.py
+++ info.py
@@ -36,7 +36,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Paramathma")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001858491982'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001981876236'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "False")), False)
