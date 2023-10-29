155df20d5965e18b08d3245a64904bf2ca49ece6
Psn <106792309+psnprasann@users.noreply.github.com>
2023-10-29 09:48:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,7 +14,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '15432910'))
 API_HASH = environ.get('API_HASH', 'bf3b5a92497cf5744e6310f309b5490a')
-BOT_TOKEN = environ.get('BOT_TOKEN', '6086474306:AAHGu0mynzmRV7xK2JuTO3nVi03wpBn14ks')
+BOT_TOKEN = environ.get('BOT_TOKEN', '6086474306:AAHRypOMl3jMpskuyFNH1EOtOEjTJPhv_Oo')
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
